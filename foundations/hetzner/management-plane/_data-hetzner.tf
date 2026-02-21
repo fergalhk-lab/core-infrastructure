f27277890067e63369eaee7da40f9c866524fffe
fhke <fhke@protonmail.com>
2026-02-21 17:00:45 +0000

    use selector for image

--- foundations/hetzner/management-plane/_data-hetzner.tf
+++ foundations/hetzner/management-plane/_data-hetzner.tf
@@ -1,4 +1,4 @@
 data "hcloud_image" "management_k8s_snapshot" {
-  name              = local.management_k8s.image_name
+  with_selector     = format("instance=k3s,version=%s", local.management_k8s.image_version)
   with_architecture = "x86"
 }
--- foundations/hetzner/management-plane/locals.tf
+++ foundations/hetzner/management-plane/locals.tf
@@ -1,7 +1,7 @@
 locals {
   management_k8s = {
-    name       = "management-k8s"
-    image_name = "k3s-20260221155452"
-    size       = "cx23"
+    name          = "management-k8s"
+    image_version = "20260221155452"
+    size          = "cx23"
   }
 }
