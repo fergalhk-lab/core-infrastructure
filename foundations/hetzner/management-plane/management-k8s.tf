c8d370d487b44488592d14fba1e90e6174b3d6e5
fhke <fhke@protonmail.com>
2026-02-21 18:43:17 +0000

    rework network

--- foundations/hetzner/management-plane/management-k8s.tf
+++ foundations/hetzner/management-plane/management-k8s.tf
@@ -11,18 +11,6 @@ locals {
   }
 }
 
-resource "hcloud_network" "management" {
-  name     = "management"
-  ip_range = "10.0.0.0/20"
-}
-
-resource "hcloud_network_subnet" "management" {
-  network_id   = hcloud_network.management.id
-  type         = "cloud"
-  network_zone = "eu-central"
-  ip_range     = "10.0.0.0/24"
-}
-
 resource "hcloud_server" "management_k8s" {
   name        = local.management_k8s.name
   image       = data.hcloud_image.management_k8s_snapshot.id
