569ae13260b589301cf9c43842b3411624967b1f
fhke <fhke@protonmail.com>
2026-02-21 22:20:12 +0000

    rebuild mgmt plane

--- foundations/hetzner/management-plane/k8s.tf
+++ foundations/hetzner/management-plane/k8s.tf
@@ -1,7 +1,7 @@
 locals {
   management_k8s = {
     name          = "management-k8s"
-    image_version = "20260221214225"
+    image_version = "20260221221507"
     size          = "cx23"
     location      = "nbg1" // Nuremberg
     apiserver_endpoint = {
