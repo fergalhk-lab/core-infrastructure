8a13a8f14705d6d2e5a1d3d2236fd6668537e58c
fhke <fhke@protonmail.com>
2026-02-21 20:23:32 +0000

    rebuild server

--- foundations/hetzner/management-plane/management-k8s.tf
+++ foundations/hetzner/management-plane/management-k8s.tf
@@ -1,12 +1,12 @@
 locals {
   management_k8s = {
     name          = "management-k8s"
-    image_version = "20260221180944"
+    image_version = "20260221185753"
     size          = "cx23"
     location      = "nbg1" // Nuremberg
     endpoint = {
       zone      = "fergal.website"
-      subdomain = "management.k8s"
+      subdomain = "k8s.management"
     }
   }
 }
@@ -17,6 +17,14 @@ resource "hcloud_server" "management_k8s" {
   server_type = local.management_k8s.size
   location    = local.management_k8s.location
   ssh_keys    = [for ssh_key in hcloud_ssh_key.management : ssh_key.id]
+  user_data   = <<EOD
+#cloud-config
+write_files:
+- path: /etc/k3s-hostname
+  owner: root:root
+  permissions: '0644'
+  content: "${local.management_k8s.endpoint.subdomain}.${local.management_k8s.endpoint.zone}"
+EOD
 
   network {
     network_id = hcloud_network.management.id
