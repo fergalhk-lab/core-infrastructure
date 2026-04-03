7c8cff3eaf3eb16b43ee9b54e666d21f4496278b
fhke <fhke@protonmail.com>
2026-04-03 21:16:12 +0100

    feat: add IAM role for external-secrets SA

--- foundation/hetzner/management/k8s-roles.tf
+++ foundation/hetzner/management/k8s-roles.tf
@@ -6,3 +6,24 @@ module "hello_world_role" {
   namespace            = "hello-world"
   service_account_name = "fergal"
 }
+
+data "aws_iam_policy_document" "external_secrets" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:DescribeSecret",
+    ]
+    resources = ["*"]
+  }
+}
+
+module "external_secrets_role" {
+  source = "./modules/pod-role"
+
+  oidc_provider_arn    = aws_iam_openid_connect_provider.management_k8s.arn
+  oidc_issuer_url      = aws_iam_openid_connect_provider.management_k8s.url
+  namespace            = "external-secrets"
+  service_account_name = "external-secrets"
+  policy_document      = data.aws_iam_policy_document.external_secrets.json
+}
