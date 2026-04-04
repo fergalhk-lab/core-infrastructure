1f1af1eaa87b881e279e8f8879d7ae155f990f2b
fhke <fhke@protonmail.com>
2026-04-04 21:00:38 +0100

    fix: clean up bootstrap_k3s_args quoting, document primary_disk_size

--- foundation/hetzner/management/k8s.tf
+++ foundation/hetzner/management/k8s.tf
@@ -15,12 +15,12 @@ locals {
   }
 
   bootstrap_k3s_args = join(" ", [
-    "'${local.management_k8s.apiserver_endpoint.subdomain}.${local.management_k8s.apiserver_endpoint.zone}'",
-    "'${local.management_k8s.anonymous_issuer_endpoint.subdomain}.${local.management_k8s.anonymous_issuer_endpoint.zone}'",
-    "'${aws_iam_role.ecr_pull.arn}'",
-    "'${hcloud_volume.management_k8s["etcd"].id}'",
-    "'${hcloud_volume.management_k8s["tls"].id}'",
-    "'${hcloud_volume.management_k8s["pvs"].id}'",
+    "${local.management_k8s.apiserver_endpoint.subdomain}.${local.management_k8s.apiserver_endpoint.zone}",
+    "${local.management_k8s.anonymous_issuer_endpoint.subdomain}.${local.management_k8s.anonymous_issuer_endpoint.zone}",
+    aws_iam_role.ecr_pull.arn,
+    hcloud_volume.management_k8s["etcd"].id,
+    hcloud_volume.management_k8s["tls"].id,
+    hcloud_volume.management_k8s["pvs"].id,
   ])
 }
 
@@ -29,7 +29,7 @@ resource "hcloud_server" "management_k8s" {
   image             = data.hcloud_image.management_k8s_snapshot.id
   server_type       = local.management_k8s.size
   location          = local.management_k8s.location
-  primary_disk_size = 10
+  primary_disk_size = 10 # minimal root disk; persistent data lives on attached volumes
   ssh_keys          = [for ssh_key in hcloud_ssh_key.management : ssh_key.id]
   # user_data is applied at first boot only. If the server is replaced by Terraform,
   # the volume IDs embedded here will reflect the current volumes (correct).
