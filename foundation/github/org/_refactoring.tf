dfcf46a0368639dc18a0bd0b134a9950f7c810a3
fhke <fhke@protonmail.com>
2026-04-04 19:30:52 +0100

    chore: import existing github repos into terraform state

---
+++ foundation/github/org/_refactoring.tf
@@ -0,0 +1,16 @@
+# Delete this file after the import has been applied.
+
+import {
+  to = github_repository.repos["core-infrastructure"]
+  id = "core-infrastructure"
+}
+
+import {
+  to = github_repository.repos["apps"]
+  id = "apps"
+}
+
+import {
+  to = github_repository.repos["deployer"]
+  id = "deployer"
+}
