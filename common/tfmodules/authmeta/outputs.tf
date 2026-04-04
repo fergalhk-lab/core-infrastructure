89e4d4e2ffc99e0e7b8abc9036c309c828bf27cf
fhke <fhke@protonmail.com>
2026-04-04 18:22:17 +0100

    feat(authmeta): add GitHub App credential outputs

--- common/tfmodules/authmeta/outputs.tf
+++ common/tfmodules/authmeta/outputs.tf
@@ -7,3 +7,18 @@ output "hetzner_keys" {
   value     = { for project, secret in data.aws_secretsmanager_secret_version.hetzner : project => secret.secret_string }
   sensitive = true
 }
+
+output "github_app_id" {
+  value     = var.enable_github ? lookup(local.github_secret, "app_id", null) : null
+  sensitive = true
+}
+
+output "github_app_installation_id" {
+  value     = var.enable_github ? lookup(local.github_secret, "installation_id", null) : null
+  sensitive = true
+}
+
+output "github_app_private_key" {
+  value     = var.enable_github ? lookup(local.github_secret, "private_key", null) : null
+  sensitive = true
+}
