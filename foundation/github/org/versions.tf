ff475a76d241720ab4b15994e40873bef06eb8b1
fhke <fhke@protonmail.com>
2026-04-04 18:27:57 +0100

    feat: add foundation/github/org terraform root module

---
+++ foundation/github/org/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
