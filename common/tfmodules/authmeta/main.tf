f4a9662458e87ffa0758d19c6d6789e21c49df84
fhke <fhke@protonmail.com>
2026-04-04 18:19:19 +0100

    feat(authmeta): add GitHub App credentials support

--- common/tfmodules/authmeta/main.tf
+++ common/tfmodules/authmeta/main.tf
@@ -11,3 +11,12 @@ data "aws_secretsmanager_secret_version" "hetzner" {
   for_each  = var.enabled_hetzner_projects
   secret_id = "apikeys/hetzner/${each.key}"
 }
+
+data "aws_secretsmanager_secret_version" "github" {
+  count     = var.enable_github ? 1 : 0
+  secret_id = "apikeys/github/terraform-core-infrastructure"
+}
+
+locals {
+  github_secret = var.enable_github ? jsondecode(data.aws_secretsmanager_secret_version.github[0].secret_string) : {}
+}
