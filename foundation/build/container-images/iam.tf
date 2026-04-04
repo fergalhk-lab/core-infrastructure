5e7a98f89be59a4ccf4c97c286d7de37b8ef41a4
fhke <fhke@protonmail.com>
2026-04-04 16:38:51 +0100

    feat: add GitHub OIDC IAM roles for ECR push

--- foundation/build/container-images/iam.tf
+++ foundation/build/container-images/iam.tf
@@ -32,3 +32,73 @@ locals {
     }
   }
 }
+
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+data "aws_iam_policy_document" "github_ecr_assume_role" {
+  for_each = local._github_repo_configs
+
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [data.aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = each.value.branch_conditions
+    }
+  }
+}
+
+resource "aws_iam_role" "github_ecr" {
+  for_each           = local._github_repo_configs
+  name               = "gh-ecr-${replace(each.key, "/", "-")}"
+  assume_role_policy = data.aws_iam_policy_document.github_ecr_assume_role[each.key].json
+}
+
+data "aws_iam_policy_document" "github_ecr" {
+  for_each = local._github_repo_configs
+
+  statement {
+    effect    = "Allow"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:PutImage",
+    ]
+    resources = [
+      for repo in each.value.ecr_repos :
+      "arn:aws:ecr:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:repository/${repo}"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "github_ecr" {
+  for_each = local._github_repo_configs
+  role     = aws_iam_role.github_ecr[each.key].name
+  policy   = data.aws_iam_policy_document.github_ecr[each.key].json
+}
