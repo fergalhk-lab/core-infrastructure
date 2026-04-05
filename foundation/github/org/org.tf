fa26b7e06282ab15f62efd1e7de568069d1736a6
Fergal Kearns <159787545+fergalhk@users.noreply.github.com>
2026-04-05 16:48:32 +0100

    Feat argocd webhook (#46)
    * feat: add random provider to github/org stack
    
    * feat: add argocd webhook secret and github webhook for k8s-deployments
    
    * feat: configure argocd webhook secret from aws secrets manager
    
    * fix: remove namespace param from argocd chart
    
    ---------
    
    Co-authored-by: fhke <fhke@protonmail.com>

--- foundation/github/org/org.tf
+++ foundation/github/org/org.tf
@@ -20,3 +20,31 @@ resource "github_repository" "repos" {
 
   delete_branch_on_merge = true
 }
+
+resource "random_password" "argocd_webhook_secret" {
+  length  = 32
+  special = false
+}
+
+resource "aws_secretsmanager_secret" "argocd_webhook_secret" {
+  name = "argocd/webhook-secret"
+}
+
+resource "aws_secretsmanager_secret_version" "argocd_webhook_secret" {
+  secret_id     = aws_secretsmanager_secret.argocd_webhook_secret.id
+  secret_string = random_password.argocd_webhook_secret.result
+}
+
+resource "github_repository_webhook" "argocd_k8s_deployments" {
+  repository = github_repository.repos["k8s-deployments"].name
+
+  configuration {
+    url          = "https://argocd.fergal.website/api/webhook"
+    content_type = "application/json"
+    secret       = random_password.argocd_webhook_secret.result
+    insecure_ssl = false
+  }
+
+  active = true
+  events = ["push"]
+}
